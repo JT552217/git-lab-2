11f3c35c1debe01cdb030be9e73504caede65f05
JT552217 <jt552217@ohio.edu>
2020-05-14 13:22:46 -0400

    Initial commit for program

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/**
+* @file: git-lab-program.cc
+* @author: Jason Tischler
+* @date: May 14, 2020
+* @brief: basic Hello World file
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!" << endl;
+
+    return 0;
+}
